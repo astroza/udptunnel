f3475de94fb008a2c90714aff9f8e97a94a9e059
Felipe Astroza <felipe@astroza.cl>
2013-08-14 18:43:08 -0400

    test

--- scripts/xnu_client.sh
+++ scripts/xnu_client.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 ifconfig utun0 10.0.0.2 10.0.0.1 mtu 1400 up
-old_default=$(route get default |grep gateway|awk '{print $2}')
-provider=$1
-route add -host $provider $old_default
-route delete default
-route add default 10.0.0.1
+#old_default=$(route get default |grep gateway|awk '{print $2}')
+#provider=$1
+#route add -host $provider $old_default
+#route delete default
+#route add default 10.0.0.1
 
 # El portal cautivo de Entel tiene un servidor DNS limitado, solo responde a 
 # direcciones frecuentes, populares, como facebook o google.
 
 # Lo cambio al DNS de Google 8.8.8.8
-for state in `echo 'list State:/Network/Service/[^/]+/DNS' | /usr/sbin/scutil | /usr/bin/awk '{print $4}'`; do
-        (echo "get $state"; echo "d.add ServerAddresses * 8.8.8.8"; echo "set $state") | /usr/sbin/scutil
+#for state in `echo 'list State:/Network/Service/[^/]+/DNS' | /usr/sbin/scutil | /usr/bin/awk '{print $4}'`; do
+#        (echo "get $state"; echo "d.add ServerAddresses * 8.8.8.8"; echo "set $state") | /usr/sbin/scutil
 done
